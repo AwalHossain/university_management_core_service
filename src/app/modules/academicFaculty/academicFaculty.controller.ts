aa67c9518fdedbcb542014ff4c2ac7e24e832bad
AwalHossain <waqin21@gmail.com>
2023-08-20 11:26:16 +0600

    feat: academicFaculty controller function ✍️

--- src/app/modules/academicFaculty/academicFaculty.controller.ts
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
+import { paginationFields } from "../../../constants/pagination";
 import catchAsync from "../../../shared/catchAsync";
+import pick from "../../../shared/pick";
 import sendResponse from "../../../shared/sendResponse";
-
+import { academicFacultyFilterableFields } from "./academicFaculty.constant";
 
 const insertIntoDB = catchAsync(async (req:Request, res:Response) => {
     const result = await acdemicFaculty.insertIntoDB(req.body);
@@ -20,7 +22,7 @@ const insertIntoDB = catchAsync(async (req:Request, res:Response) => {
 
 
 const getAll= catchAsync(async (req: Request, res: Response) => {
-    const filter = pick(req.query, academicDepartmentFilterableFields);
+    const filter = pick(req.query, academicFacultyFilterableFields);
     const options = pick(req.query, paginationFields);
 
     const result = await acdemicFaculty.getAll(filter, options);
