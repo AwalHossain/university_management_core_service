9d7335ac5b5823664dcd42335e1b8cd8f02beb70
AwalHossain <waqin21@gmail.com>
2023-09-25 05:08:31 +0600

    feat: Add access only specific user role only 🛡️

--- src/app/modules/building/building.route.ts
+++ src/app/modules/building/building.route.ts
@@ -1,5 +1,7 @@
 
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { BuildingController } from './building.controller';
 import { BuildingValidation } from './building.validation';
@@ -9,6 +11,7 @@ const router = express.Router();
 
 router.post(
     '/',
+    auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
     validateRequest(BuildingValidation.create),
     BuildingController.insertIntoDB
 )
