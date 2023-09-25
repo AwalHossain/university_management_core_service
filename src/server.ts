7ff45cc8324841687b19ca90bf03dd387172637a
AwalHossain <waqin21@gmail.com>
2023-09-25 14:46:15 +0600

    refactor: commented out uncaught exceptions and unhandled rejections 🔴

--- src/server.ts
+++ src/server.ts
@@ -33,8 +33,8 @@ async function bootstrap() {
     exitHandler();
   };
 
-  process.on('uncaughtException', unexpectedErrorHandler);
-  process.on('unhandledRejection', unexpectedErrorHandler);
+  // process.on('uncaughtException', unexpectedErrorHandler);
+  // process.on('unhandledRejection', unexpectedErrorHandler);
 
   process.on('SIGTERM', () => {
     logger.info('SIGTERM received');
