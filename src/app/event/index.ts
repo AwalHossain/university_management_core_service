3e8d33f2ecb7f4ceadbe3a6511bf221cc61cf376
AwalHossain <waqin21@gmail.com>
2023-09-25 14:46:47 +0600

    feat: new event for faculty 🆕

--- src/app/event/index.ts
+++ src/app/event/index.ts
@@ -1,3 +1,4 @@
+import initFacultyEvent from "../modules/faculty/faculty.event";
 import initStudentEvent from "../modules/student/student.event";
 
 
@@ -5,6 +6,7 @@ import initStudentEvent from "../modules/student/student.event";
 
 const subscribeEvent = async () => {
     await initStudentEvent();
+    await initFacultyEvent();
 }
 
 
---
+++ src/app/modules/faculty/faculty.event.ts
@@ -0,0 +1,27 @@
+import { RedisClient } from "../../../shared/redis"
+import { FACULTY_EVENT_CREATED, FACULTY_EVENT_DELETED, FACULTY_EVENT_UPDATED } from "./faculty.constant"
+import { FacultyService } from "./faculty.service"
+
+
+
+const initFacultyEvent = async () => {
+    RedisClient.subscribe(FACULTY_EVENT_CREATED, async (e) => {
+        const data = JSON.parse(e)
+        console.log(data, 'faculty from event');
+
+        await FacultyService.createFacultyEvent(data)
+    })
+
+    RedisClient.subscribe(FACULTY_EVENT_UPDATED, async (e) => {
+        const data = JSON.parse(e)
+
+        await FacultyService.updateFacultyFromEvent(data);
+    })
+
+    RedisClient.subscribe(FACULTY_EVENT_DELETED, async (e) => {
+        console.log(e)
+    })
+}
+
+
+export default initFacultyEvent;
